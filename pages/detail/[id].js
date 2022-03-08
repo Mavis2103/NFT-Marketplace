029d7c86a4bbf65b4064666b26a447a629638a75
Huy19IT1 <huy2001nd@gmail.com>
2022-03-09 00:58:45 +0700

    update timer

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,20 +1,65 @@
 
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRotateRight, faArrowUpRightFromSquare, faShareNodes, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 export default function id() {
 
     const router = useRouter()
     const { id } = router.query
+    const now = new Date()
+
+    const calculateTimeLeft = () => {
+        let year = new Date().getFullYear();
+        const difference = +new Date(`2022-03-11`) - +new Date();
+        let timeLeft = {};
+
+        if (difference >= 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                mins: Math.floor((difference / 1000 / 60) % 60),
+                secs: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+    });
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach((interval) => {
+        if (!timeLeft[interval]) {
+            return;
+        }
+
+        timerComponents.push(
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                <span className="font-mono text-5xl w-14">
+                    <span>{timeLeft[interval]}</span>
+                </span>
+               {interval}
+            </div>
+        );
+    });
 
     return (
         <main className="px-52 my-10">
             <div className='grid grid-cols-12'>
-                <div className='col-span-5 mr-10'>
+                <div className='col-span-5 mr-10 flex flex-col'>
                     <div className=' w-full h-full'>
                         <div className="card w-full bg-base-100 shadow-xl border-2 border-black">
                             <figure>
-                                <img className='object-cover w-full' src="https://api.lorem.space/image/shoes?h=600" alt="Shoes"/>
-                                    </figure>
+                                <img className='object-cover w-full' src="https://api.lorem.space/image/shoes?h=600" alt="Shoes" />
+                            </figure>
                             <div className="card-body">
                                 <div className="card-actions justify-end">
                                     <div className="badge badge-outline">Fashion</div>
@@ -23,58 +68,77 @@ export default function id() {
                             </div>
                         </div>
                     </div>
+                    <div className='flex flex-col w-full border-2 border-black rounded-lg mt-5'>
+                        <div tabindex="0" className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-lg">
+                            <input type="checkbox" className="peer" />
+                            <div className="collapse-title text-xl font-medium bg-gray-700">
+                                Description
+                            </div>
+                            <div className="collapse-content">
+                                <p className='font-thin text-base pt-5'>
+                                    Created by
+                                    1FBF26
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className='col-span-7'>
                     <div className='w-full h-96'>
-                    <div className='w-full h-20'>
-                        <div className='flex flex-row '>
-                        <span className='text-2xl font-medium text-gray-300 w-3/4'>
-                            Collection
-                        </span>
-                                <div className="btn-group w-1/4 ">
-                                    <button className="btn border-2 border-gray-400">B</button>
-                                    <button className="btn border-2 border-gray-400">B</button>
-                                    <button className="btn border-2 border-gray-400">B</button>
-                                    <button className="btn border-2 border-gray-400">B</button>
+                        <div className='w-full h-20'>
+                            <div className='flex flex-row justify-between items-center'>
+                                <div className='text-2xl font-medium text-gray-300'>
+                                    Collection
+                                </div>
+                                <div className="btn-group">
+                                    <button className="btn border-2 border-gray-400 gap-1 peer"><FontAwesomeIcon icon={faArrowRotateRight} /></button>
+                                    <button className="btn border-2 border-gray-400 gap-1"><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></button>
+                                    <button className="btn border-2 border-gray-400 gap-1"><FontAwesomeIcon icon={faShareNodes} /></button>
+                                    <button className="btn border-2 border-gray-400 gap-1"><FontAwesomeIcon icon={faEllipsisVertical} /></button>
+                                </div>
                             </div>
-                        </div>
-                        <div className='flex flex-col'>
-                            <span className='text-4xl font-medium text-white w-full mb-3'>
-                                What F*** this Shoes #xxx
-                            </span>
-                            <span className='text-base font-thin flex flex-row'>
+                            <div className='flex flex-col'>
+                                <span className='text-4xl font-medium text-white w-full mb-3'>
+                                    What F*** this Shoes #xxx
+                                </span>
+                                <span className='text-base font-thin flex flex-row'>
                                     Owned by UserXXX
-                                    
+
                                     <span className='ml-10 w-40 flex flex-row'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                                    </svg>
-                                    15 favorites
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                                        </svg>
+                                        15 favorites
                                     </span>
-                            </span>
-                        </div>
+                                </span>
+                            </div>
                             <div className='flex flex-col w-full border-2 border-black rounded-lg mt-5'>
                                 <div className='px-5 py-5 bg-gray-700 rounded-lg'>
-                                <span>
-                                Sale ends March 11, 2022 at 10:48am +07 
-                                </span>
+                                    <div className='mb-5'>
+                                        Sale ends March 11, 2022
+                                    </div>
+                                    <div className="flex flex-row gap-5 text-center">
+                                       
+                                        {timerComponents}
+
+                                    </div>
                                 </div>
                                 <div className='px-5 py-5 flex flex-col'>
                                     <span className='font-semibold text-xl'>
                                         Current price
-                                   </span>
-                                   <span className='font-bold text-3xl text-white flex flex-row  items-end'>
+                                    </span>
+                                    <span className='font-bold text-3xl text-white flex flex-row  items-end'>
                                         <img alt="ETH" className='w-8 h-7' src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"></img>
                                         0.03
-                                    <span className='font-thin text-base text-slate-300 ml-3'>($75.39)</span>
-                                   </span>
+                                        <span className='font-thin text-base text-slate-300 ml-3'>($75.39)</span>
+                                    </span>
                                     <div className='mt-5'>
                                         <button className="btn btn-info btn-wide gap-2 ">
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
                                                 <path fillRule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clipRule="evenodd" />
-                                            </svg> 
-                                           <span className='font-sans'>Buy now</span>
+                                            </svg>
+                                            <span className='font-sans'>Buy now</span>
                                         </button>
                                         <button className="btn btn-wide gap-2 ml-5">
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -85,8 +149,8 @@ export default function id() {
                                     </div>
 
                                 </div>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
