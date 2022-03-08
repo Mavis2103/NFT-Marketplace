a8b46c7a272cde9c698e20f545f1bd6c443751db
Khale <lkha.5023@gmail.com>
2022-03-08 22:44:27 +0700

    Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./containers/**/*.{html,js}"
-    // , "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  plugins: [require("daisyui")]
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./containers/**/*.{html,js}"
+        // , "./components/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        screens: {
+            '2xs': '320px',
+            'xs': '560px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        }
+    },
+    plugins: [require("daisyui")]
 };
