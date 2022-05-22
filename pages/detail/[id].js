c27b736d8b83b081841eed116fafca882276b35c
Huy19IT1 <huy2001nd@gmail.com>
2022-05-22 10:36:04 +0700

    Update [id].js

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -239,7 +239,6 @@ export default function id() {
                           </svg>
                           <span className="font-sans">Sell</span>
                         </label>
-
                         <input
                           type="checkbox"
                           id="resell-modal"
@@ -292,8 +291,26 @@ export default function id() {
                           </label>
                         </label>
                       </>
-                    ) : (
-                      <button className="btn btn-info btn-wide gap-2 ">
+                    ) : nft.seller === info?.address ? (
+                        <button className="btn btn-disabled btn-info btn-wide gap-2 ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor">
+                          <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+                          <path
+                            fillRule="evenodd"
+                            d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        <span className="font-sans">
+                          Buy now 
+                        </span>
+                      </button>
+                      ) : (
+                         <button className="btn btn-info btn-wide gap-2 ">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           className="h-5 w-5"
@@ -310,7 +327,7 @@ export default function id() {
                           Buy now
                         </span>
                       </button>
-                    )}
+                      ) }
                   </div>
                 </div>
               </div>
