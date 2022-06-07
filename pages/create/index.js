209372eccd490c8e4fe589b54af62fefa5488312
Marca01 <61070803+Marca01@users.noreply.github.com>
2022-06-07 17:10:15 +0000

    add some loading

--- pages/create/index.js
+++ pages/create/index.js
@@ -86,7 +86,7 @@ export default function Create() {
   console.log("edited create", state.editedImg);
 
   return state.isLoading ? (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-400">
+    <div className="fixed top-0 right-0 bottom-0 left-0 z-50 flex justify-center pb-5">
       <FontAwesomeIcon icon={faSpinner} size="10x" spin />
     </div>
   ) : (
--- pages/home/index.js
+++ pages/home/index.js
@@ -70,7 +70,7 @@ const Home = props => {
   };
 
   return isLoading ? (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-400">
+    <div className="fixed top-0 right-0 bottom-0 left-0 z-50 flex justify-center pb-5">
       <FontAwesomeIcon icon={faSpinner} size="10x" spin />
     </div>
   ) : (
