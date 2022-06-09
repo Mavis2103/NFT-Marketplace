d56a9a1923454a49de76c2e144609ff40a97f41d
Marca01 <61070803+Marca01@users.noreply.github.com>
2022-06-09 15:47:36 +0000

    test rebase on Khaleee

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,7 +5,7 @@ const About = props => {
   return (
     <div className="text-center my-10 py-10">
       <p className="text-5xl font-bold mb-3">Vigor NFT Marketplace</p>
-      <p className="text-xl font-bold">Made by Le Van Kha - Tran Anh Quan</p>
+      <p className="text-xl font-bold">Made by Le Van Kha & Tran Anh Quan</p>
     </div>
   );
 };
